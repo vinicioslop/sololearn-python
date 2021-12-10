b0b22c4db15f8fff4280d0f16d7eb1d3bd7d686f
vinicioslop <vinicioslop7@hotmail.com>
2021-12-10 12:12:59 -0300

    Importa os códigos feitos no segundo módulo

---
+++ Projetos/Calculadora.py
@@ -0,0 +1,18 @@
+# Simple Calculator
+# 
+# Write a program to take two integers as input and output their sum.
+# 
+# Sample Input:
+# 2
+# 8
+# 
+# Sample Output:
+# 10
+# 
+# Remember, input() results in a string.
+
+# your code goes here
+num01 = int(input())
+num02 = int(input())
+
+print(num01 + num02)
---
+++ Quizes/Quiz02.py
@@ -0,0 +1,35 @@
+# PERGUNTA 01
+# What is the output of this code?
+spam = "7"
+spam = spam + "0"
+eggs = int(spam) + 3
+print(float(eggs))
+
+# PERGUNTA 02
+# What is the output of this code if the user enter '42' as input:
+age = int(input())
+print(age+8)
+
+# PERGUNTA 03
+# What is the output of this code?
+x = 5
+y = x + 3
+y = int(str(y) + "2")
+print(y)
+
+# PERGUNTA 04
+# Fill in the blanks to declare a variable, add 5 to it and print its value.
+x = 4
+x += 5
+print(x)
+
+# PERGUNTA 05
+# What is the output of this code?
+x = 3
+num = 17
+print(num % x)
+
+# PERGUNTA 06
+# Fill in the blanks to take a name as input and output a Welcome message, containing the input?
+name = input()
+print("Welcome, " + name)
